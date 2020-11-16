e76259ef8e8faed4657e61cb6c3fe1353e8e9f76
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-11-16 16:59:44 -0300

    fixed port

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333 || process.env.PORT, () => console.log('Server running'))
+app.listen(process.env.PORT || 3333, () => console.log('Server running'))
